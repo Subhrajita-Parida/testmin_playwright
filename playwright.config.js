26fd0882caba4f77653f2b3eb4f2eec1db1c6464
Subhrajita-Parida <subhrajita.parida@feuji.com>
2024-12-30 22:34:39 +0530

    updated code

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig({
     trace: 'on-first-retry',
     video: 'on',
     screenshot: 'on',
-    headless:false
+    headless:true
   },
 
   /* Configure projects for major browsers */
